52f0da2704edd22c76f649fcaa60a03a092a4b6e
Max <titantbx1989215@gmail.com>
2015-08-25 22:40:17 +1200

    a

--- project2/sendMessage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-
-#curl -i -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d @example.json http://54.153.185.41:80/api
-curl -i -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d @example.json http://54.153.185.41:800/api
